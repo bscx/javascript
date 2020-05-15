4e8c093cf480704349a8cd59a9837f6b7bddcb67
bsc <bsc@4xs.org>
2020-05-15 07:43:00 +0200

    Played around with the syntax of function calls

--- greaterThan.js
+++ greaterThan.js
@@ -1,5 +1,9 @@
 function greaterThan(n) {
+    console.log(n);
     return m => m > n;
 }
 
-console.log(greaterThan(10)(11));
+// console.log((greaterThan(10))(11));
+
+let newGreaterThan = greaterThan(10);
+console.log(newGreaterThan(11));
